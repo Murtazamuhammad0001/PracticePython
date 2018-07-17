446451ba1573bb4a74016c3b6a5a9b6926dbfbe7
MURTAZAMUHAMMAD\BD <murtazamuhammad52@gmail.com>
2018-07-17 10:37:05 +0500

    Initial Commit

---
+++ my.py
@@ -0,0 +1,13 @@
+fname = 'Murtaza'
+lastName = 'Muhammad'
+age = 23
+
+print("Name: "+ fname +" "+ ("Father's Name: " +(lastName) +" "+ ("age: " +str(age))))
+
+print(1 + 1)
+print(1 - 1)
+print(1 * 1)
+print(1 / 1)
+print("1"+"1")
+print(2**2)
+print(4//2)
